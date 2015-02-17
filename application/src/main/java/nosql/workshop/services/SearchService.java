3e75f9ce6dfce9b4c41ce72e9285375394bc62d4
MasterSkimo <gutimatt@hotmail.fr>
2015-02-17 15:34:29 +0100

    toLower for research

--- application/src/main/java/nosql/workshop/services/SearchService.java
+++ application/src/main/java/nosql/workshop/services/SearchService.java
@@ -86,7 +86,7 @@ public class SearchService {
         SearchResponse response = elasticSearchClient.prepareSearch("towns")
                 .setTypes("town")
                 .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
-                .setQuery(QueryBuilders.wildcardQuery("townName", townName+"*"))
+                .setQuery(QueryBuilders.wildcardQuery("townName", townName.toLowerCase()+"*"))
                 .setExplain(true)
                 .execute()
                 .actionGet();
